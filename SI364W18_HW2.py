77810e3f036ca29fbab95d0b17d4c4d8a4c78e82
Olivia Gardella <gardella@umich.edu>
2018-09-26 17:49:02 -0400

    Started working on hw2

--- SI364W18_HW2.py
+++ SI364W18_HW2.py
@@ -1,10 +1,14 @@
 ## SI 364
 ## Winter 2018
 ## HW 2 - Part 1
+## Olivia Gardella
 
-## This homework has 3 parts, all of which should be completed inside this file (and a little bit inside the /templates directory).
+## This homework has 3 parts, all of which should be completed inside this file (and a little bit
+## inside the /templates directory).
 
-## Add view functions and any other necessary code to this Flask application code below so that the routes described in the README exist and render the templates they are supposed to (all templates provided are inside the templates/ directory, where they should stay).
+## Add view functions and any other necessary code to this Flask application code below so that
+## the routes described in the README exist and render the templates they are supposed to (all
+## templates provided are inside the templates/ directory, where they should stay).
 
 ## As part of the homework, you may also need to add templates (new .html files) to the templates directory.
 
@@ -34,14 +38,37 @@ app.config['SECRET_KEY'] = 'hardtoguessstring'
 ###### ROUTES ######
 ####################
 
+# in original file - do not change
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-
+# in original file - do not change
 @app.route('/user/<name>')
 def hello_user(name):
     return '<h1>Hello {0}<h1>'.format(name)
+
+#
+@app.route('/artist_info')
+def artist_info:
+    pass
+
+#
+@app.route('/artist_links')
+def artist_links:
+    pass
+
+#
+@app.route('/artistform')
+def artistform:
+    pass
+
+#
+@app.route('/specific_artist')
+def specific_artist:
+    pass
+
+
 
 
 if __name__ == '__main__':
